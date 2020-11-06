bb296114170a560a384dc9d756484a7eed1a30a6
ZLL <15880281+zll-test@users.noreply.github.com>
2020-11-05 16:36:25 -0800

    First commit

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            Content = new StackLayout
+            {
+                //Margin = new Thickness(20),
+
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+
+                Children =
+                {
+                    new Label { Text = "Hello World" },
+                    new Slider { }
+                }
+            };
+        }
+
+    }
+}
